09d9457b5b5214401eec9d64e751ed37a3ac5f96
KrinklyDinkly <isaiah.rodriguez0207@gmail.com>
2023-10-18 12:01:34 -0700

    Update project

--- Prototype 3 - Sounds and Effects/Assets/Scripts/MoveLeft.cs
+++ Prototype 3 - Sounds and Effects/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float _speed = 30f;
+    private PlayerController _playercontrollerscript;
+    private float _leftbound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playercontrollerscript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        if(_playercontrollerscript.GameOver == false)
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        if(transform.position.x < _leftbound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype 3 - Sounds and Effects/Assets/Scripts/SpawnManager.cs
+++ Prototype 3 - Sounds and Effects/Assets/Scripts/SpawnManager.cs
@@ -8,17 +8,22 @@ public class SpawnManager : MonoBehaviour
 
     private float _startDelay = 2f;
     private float _repeatRate = 2f;
+    private PlayerController _playerControllerScript;
 
     private Vector3 _spawnPosition = new Vector3(30, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", _startDelay, _repeatRate);
     }
 
    private void SpawnObstacle()
     {
-        Instantiate(ObstaclePrefab, _spawnPosition, ObstaclePrefab.transform.rotation);
+        if(_playerControllerScript.GameOver == false)
+        {
+            Instantiate(ObstaclePrefab, _spawnPosition, ObstaclePrefab.transform.rotation);
+        }
     }
 }
